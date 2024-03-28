57691c172120bd1589c81025237d72e6d20a0ea9
bachhuy <112871140+bachhuy@users.noreply.github.com>
2024-03-28 17:25:54 +0700

    Add files via upload

---
+++ btvn-biến/bien.js
@@ -0,0 +1,13 @@
+//let myNumber = 10;
+    //console.log(myNumber)
+
+    //let myString = "Hello, world!";
+    //console.log(myString.length);
+
+    let isMale = true;
+    if (isMale) {
+    console.log("Người này là nam giới");
+    } 
+    else {
+    console.log("Người này không phải là nam giới");
+    }
